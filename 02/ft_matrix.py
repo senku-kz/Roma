a97ea8b014bc2d72f66a355fa9b2fc24cfede176
senku <senku.kz@gmail.com>
2023-01-25 11:26:00 +0600

    02-01

---
+++ 02/ft_matrix.py
@@ -0,0 +1,48 @@
+"""
+python ft_matrix.py 2
+"""
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Error! Usage: python3 ft_matrix.py <n> <m>")
+    else:
+        n = int(sys.argv[1])
+        m = int(sys.argv[2])
+        arr = [[0 for i in range(m)] for i in range(n)]
+
+        for i in range(n):
+            for j in range(m):
+                arr[i][j] = float(input("Insert the element in position ({}, {}): ".format(i, j)))
+
+        for i in range(n):
+            print("[", end="")
+            for j in range(m):
+                print(arr[i][j], end=" ")
+            print("]")
+
+        # The sum over rows
+        rows_sum = [0 for i in range(n)]
+        for i in range(n):
+            s = 0
+            for j in range(m):
+                s = s + arr[i][j]
+            rows_sum[i] = s
+
+        print("The sum over rows is:\n[", end="")
+        for i in range(n):
+            print(rows_sum[i], end=" ")
+        print("]")
+
+        # The sum over columns
+        column_sum = [0 for i in range(m)]
+        for j in range(m):
+            s = 0
+            for i in range(n):
+                s = s + arr[i][j]
+            column_sum[j] = s
+
+        print("The sum over columns is:\n[", end="")
+        for j in range(m):
+            print(column_sum[j], end=" ")
+        print("]")
