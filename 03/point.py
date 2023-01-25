c9ed019f77bb53356af533910a26b89d41fcd1e8
Rustem <senku.kz@gmail.com>
2023-01-26 00:32:17 +0600

    02-05

---
+++ 03/point.py
@@ -0,0 +1,16 @@
+"""
+python point.py
+python -c 'from point import Point; print(Point(1,1).x)'
+"""
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+point = Point(5, 4)
+
+"Insert the coordinates of the first point:"
+"Insert the coordinates of the second point:"
