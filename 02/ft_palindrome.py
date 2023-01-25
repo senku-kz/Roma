8599b22972ce027d12a06958abef474ff5dd4a7f
senku <senku.kz@gmail.com>
2023-01-25 10:46:57 +0600

    02-01

--- 02/ft_palindrome.py
+++ 02/ft_palindrome.py
@@ -1,12 +1,26 @@
+"""
+python ft_palindrome.py kayak
+python ft_palindrome.py exam
+python ft_palindrome.py are we not drawn onward to new era
+python ft_palindrome.py "are we not drawn onward to new era"
+python -c 'from ft_palindrome import is_palindrome; print(is_palindrome("kayak"))'
+"""
+
 import sys
+
+
+def is_palindrome(v):
+    return v == v[::-1]
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Error! Usage: python3 ft_ext_pos.py <x1> ... <xn>")
     elif len(sys.argv) > 2:
-        print("Error! Usage: python3 ft_ext_pos.py <x1> ... <xn>")
+        print("Error! Insert just 1 string!")
     else:
         s = sys.argv[1]
-        if s == s[::-1]:
-            print("Yes")
+        if is_palindrome(s):
+            print("The string {} is palindrome".format(s))
         else:
-            print("No")
+            print("The string {} is not palindrome".format(s))
