3d0232e6ca6480ac9152fe7aab6c21f7148a26a0
Rustem <senku.kz@gmail.com>
2023-01-26 00:20:01 +0600

    02-05

---
+++ ex02/ft_char_count.py
@@ -0,0 +1,24 @@
+"""
+python ft_char_count.py programming
+python ft_char_count.py "this is a test"
+python ft_char_count.py
+"""
+
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Error! No string given")
+    else:
+        s = sys.argv[1]
+        d = {}
+        for c in s:
+            if c in d:
+                d[c] += 1
+            else:
+                d[c] = 1
+
+        t = dict(sorted(d.items()))
+        for k, v in t.items():
+            print("{} = {}".format(k, v))
