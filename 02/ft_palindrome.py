5026bd2c5535d47c8cb4f232c568d162d645f04f
senku <senku.kz@gmail.com>
2023-01-25 09:57:58 +0600

    palindrome draft

--- 02/ft_palindrome.py
+++ 02/ft_palindrome.py
@@ -1,6 +1,8 @@
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Error! Usage: python3 ft_ext_pos.py <x1> ... <xn>")
+    elif len(sys.argv) > 2:
+        print("Error! Usage: python3 ft_ext_pos.py <x1> ... <xn>")
     else:
         s = sys.argv[1]
         if s == s[::-1]:
