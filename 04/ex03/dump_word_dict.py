10b225aca8d517906c9f2ae2fe1cd3606e63f86b
senku <senku.kz@gmail.com>
2023-01-26 16:31:24 +0600

    04-03

---
+++ 04/ex03/dump_word_dict.py
@@ -0,0 +1,20 @@
+"""
+python dump_word_dict.py
+python -m pickle word_count.pickle
+"""
+import pickle
+
+filename = "words.txt"
+dmp_filename = "word_count.pickle"
+d = {}
+with open(filename) as f:
+    for line in f:
+        word = line.strip()
+        word_length = len(word)
+        if word_length in d:
+            d[word_length] += 1
+        else:
+            d[word_length] = 1
+
+with open(dmp_filename, "wb") as binary_file:
+    pickle.dump(d, binary_file, pickle.HIGHEST_PROTOCOL)
