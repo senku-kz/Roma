ad7ff920da40b9181cf07843626a054ecd748f7b
senku <senku.kz@gmail.com>
2023-01-26 16:38:45 +0600

    refactoring

---
+++ 00/ex04/ft_print.py
@@ -0,0 +1,4 @@
+s = input("Insert a string: ")
+n = abs(int(input("Insert an integer: ")))
+i = s[n: -n+1]
+print(i)
--- 00/ft_print_02.py
+++
@@ -1,4 +0,0 @@
-s = input("Insert a string:")
-n = abs(int(input("Insert an integer:")))
-i = s[n: -n+1]
-print(i)
