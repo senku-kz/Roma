d0394f7b920b4aae3fb7fd5d704e56a1ad5d56ca
senku <senku.kz@gmail.com>
2023-01-27 15:33:07 +0600

    remove class attributes

--- 03/ex03/student.py
+++ 03/ex03/student.py
@@ -5,9 +5,6 @@ python -c 'from student import Student; print(Student("Stefano","Guarino"))'
 
 
 class Person:
-    # first_name = ''
-    # last_name = ''
-
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
