6bba1db3ea0b85e8919305a613585dadaa8e7516
senku <senku.kz@gmail.com>
2023-01-27 09:08:08 +0600

    refactoring

--- 02/ex00/ft_ext_pos.py
+++ 02/ex00/ft_ext_pos.py
@@ -5,16 +5,17 @@ python ft_ext_pos.py
 
 import sys
 
-if len(sys.argv) < 2:
-    print("Error! Usage: python3 ft_ext_pos.py <x1> ... <xn>")
-else:
-    lst = [int(i) for i in sys.argv[1:]]
-    min_idx = 0
-    max_idx = 0
-    for i in range(1, len(lst)):
-        if lst[min_idx] > lst[i]:
-            min_idx = i
-        if lst[max_idx] < lst[i]:
-            max_idx = i
-    print("The min is {1} and its position is {0}".format(min_idx, lst[min_idx]))
-    print("The max is {1} and its position is {0}".format(max_idx, lst[max_idx]))
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Error! Usage: python3 ft_ext_pos.py <x1> ... <xn>")
+    else:
+        lst = [int(i) for i in sys.argv[1:]]
+        min_idx = 0
+        max_idx = 0
+        for i in range(1, len(lst)):
+            if lst[min_idx] > lst[i]:
+                min_idx = i
+            if lst[max_idx] < lst[i]:
+                max_idx = i
+        print("The min is {1} and its position is {0}".format(min_idx, lst[min_idx]))
+        print("The max is {1} and its position is {0}".format(max_idx, lst[max_idx]))
