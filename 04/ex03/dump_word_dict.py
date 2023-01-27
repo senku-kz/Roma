d1f6ecaf2e31abdf06da9d6402396afa71a16b11
senku <senku.kz@gmail.com>
2023-01-27 19:59:27 +0600

    try to pprint output file

--- 04/ex03/dump_word_dict.py
+++ 04/ex03/dump_word_dict.py
@@ -2,7 +2,9 @@
 python dump_word_dict.py
 python -m pickle word_count.pickle
 """
+import json
 import pickle
+import pprint
 
 filename = "words.txt"
 dmp_filename = "word_count.pickle"
@@ -24,3 +26,8 @@ with open(dmp_filename, 'wb') as binary_file:
     pickle.dump(d, binary_file)
     # pickle.dump(d, binary_file, protocol= pickle.DEFAULT_PROTOCOL)
     # pickle.dump(d, binary_file, pickle.HIGHEST_PROTOCOL, fix_imports=True)
+
+# dmp = pickle.dumps(d)
+# print(dmp)
+# with open(dmp_filename, 'wb') as binary_file:
+#     binary_file.write(dmp)
