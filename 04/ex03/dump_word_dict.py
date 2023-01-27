9423828820baa706a84983149099b689a6341705
senku <senku.kz@gmail.com>
2023-01-27 19:26:22 +0600

    add processing manu words in one line

--- 04/ex03/dump_word_dict.py
+++ 04/ex03/dump_word_dict.py
@@ -9,12 +9,16 @@ dmp_filename = "word_count.pickle"
 d = {}
 with open(filename) as f:
     for line in f:
-        word = line.strip()
-        word_length = len(word)
-        if word_length in d:
-            d[word_length] += 1
-        else:
-            d[word_length] = 1
+        line = line.strip()
+        line = line.lower()
+        words = line.split()
+        for word in words:
+            word = word.strip()
+            word_length = len(word)
+            if word_length in d:
+                d[word_length] += 1
+            else:
+                d[word_length] = 1
 
 with open(dmp_filename, 'wb') as binary_file:
     pickle.dump(d, binary_file)
